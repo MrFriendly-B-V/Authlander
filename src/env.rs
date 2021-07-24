806204863d002e797f79ec00561d18cc3c735377
Tobias de Bruijn <t.debruijn@array21.dev>
2021-07-25 00:18:06 +0200

    Added name to api_key table

--- src/env.rs
+++ src/env.rs
@@ -26,7 +26,7 @@ pub fn migrate(conn: &mut mysql::PooledConn) -> Result<()> {
     unwrap!("states", conn.exec_drop("CREATE TABLE IF NOT EXISTS states (`state` varchar(32) PRIMARY KEY NOT NULL, `nonce` varchar(128) NOT NULL, redirect_uri TEXT NOT NULL)", Empty));
     unwrap!("users", conn.exec_drop("CREATE TABLE IF NOT EXISTS users (`user_id` varchar(255) PRIMARY KEY NOT NULL, `active` BOOLEAN NOT NULL, `name` varchar(255), `email` varchar(255), `picture` TEXT, `refresh_token` varchar(255))", Empty));
     unwrap!("sessions", conn.exec_drop("CREATE TABLE IF NOT EXISTS sessions (`session_id` varchar(32) PRIMARY KEY NOT NULL, `user_id` varchar(255) NOT NULL, expiry BIGINT)", Empty));
-    unwrap!("api_users", conn.exec_drop("CREATE TABLE IF NOT EXISTS api_users (`api_token` varchar(64) PRIMARY KEY NOT NULL, `active` BOOLEAN)", Empty));
+    unwrap!("api_users", conn.exec_drop("CREATE TABLE IF NOT EXISTS api_users (`api_token` varchar(64) PRIMARY KEY NOT NULL, `active` BOOLEAN, `name` varchar(64) NOT NULL)", Empty));
     unwrap!("scopes", conn.exec_drop("CREATE TABLE IF NOT EXISTS scopes (`id` int PRIMARY KEY NOT NULL AUTO_INCREMENT, `scope_name` varchar(32) NOT NULL, `user_id` varchar(32) NOT NULL)", Empty));
 
     Ok(())
